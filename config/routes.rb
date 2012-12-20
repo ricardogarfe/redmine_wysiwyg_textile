c622ae94d40eb8c1687c72e9e8dac39d53568ef4
Ricardo García Fernández <ricardogarfe@gmail.com>
2012-12-20 11:00:10 +0100

    Updated routes.rb with redmine configured URL

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 #custom routes for this plugin
 ActionController::Routing::Routes.draw do |map|
   map.with_options :controller => 'convert' do |convert_routes|
-    convert_routes.connect "convert/wysiwygtohtmltotextile", :conditions => { :method => [:post, :put] }, :action => 'wysiwygtohtmltotextile'
-    convert_routes.connect "convert/wysiwygtotextiletohtml", :conditions => { :method => [:post, :put] }, :action => 'wysiwygtotextiletohtml'
+    convert_routes.connect "#{Redmine::Utils.relative_url_root}/convert/wysiwygtohtmltotextile", :conditions => { :method => [:post, :put] }, :action => 'wysiwygtohtmltotextile'
+    convert_routes.connect "#{Redmine::Utils.relative_url_root}/convert/wysiwygtotextiletohtml", :conditions => { :method => [:post, :put] }, :action => 'wysiwygtotextiletohtml'
   end
 end
