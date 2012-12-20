1d80373dccef6cd188df9ad51f4d78a6b21e7495
Ricardo García Fernández <ricardogarfe@gmail.com>
2012-12-20 11:01:11 +0100

    Update routes with Redmin dinamic URL

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 #custom routes for this plugin
 RedmineApp::Application.routes.draw do
 
-    match "convert/wysiwygtohtmltotextile", :via => [:post, :put]
-    match "convert/wysiwygtotextiletohtml", :via => [:post, :put]
+    match "#{Redmine::Utils.relative_url_root}/convert/wysiwygtohtmltotextile", :via => [:post, :put]
+    match "#{Redmine::Utils.relative_url_root}/convert/wysiwygtotextiletohtml", :via => [:post, :put]
 
 end
